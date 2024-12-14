874b4e003efd776d209853b4a4ecc8b04b581951
Surajit <surajithalder088@gmail.com>
2024-12-15 02:40:49 +0530

    deployment

--- client/src/pages/peoples/people.jsx
+++ client/src/pages/peoples/people.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Topbar from '../../components/topbar/Topbar'
 import Sidebar from '../../components/sidebar/Sidebar'
 
-import Rightbar from '../../components/rightbar/rightbar'
+import Rightbar from '../../components/rightbar/rightbar';
 import './people.css'
 
 const people = () => {
@@ -14,7 +14,7 @@ const people = () => {
       <Sidebar/>
      
       <Rightbar people={1}/>
-      <Rightbar people={1}/>
+      
     </div>
     
  </>
